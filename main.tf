9c2209367fea663eb73bc363b3254e22e3c00df2
YkeLit <keyang117@outlook.com>
2024-07-02 16:14:11 +0800

    feat: headscale tf file

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    headscale = {
+      source  = "awlsring/headscale"
+      version = "0.1.7"
+    }
+  }
+}
+
+provider "headscale" {
+  endpoint = "https://neo-shuttler.jet.work"
+}
+
+data "headscale_api_keys" "active_keys" {}
